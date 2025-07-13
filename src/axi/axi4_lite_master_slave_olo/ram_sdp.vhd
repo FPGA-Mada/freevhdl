ff63c4e6d862626b509cf1004087e9546c87397e
Nambinina Rakotojaona <53557693+FPGA-Mada@users.noreply.github.com>
2025-07-13 17:36:05 +0200

    Update ram_sdp.vhd

--- src/axi/axi4_lite_master_slave_olo/ram_sdp.vhd
+++ src/axi/axi4_lite_master_slave_olo/ram_sdp.vhd
@@ -11,6 +11,7 @@ entity ram_sdp is
   );
   port (
     Clk         : in  std_logic;
+    Rst         : in  std_logic;  -- Active high reset
 
     -- AXI-like user interface
     Wr_Addr     : in  std_logic_vector(Add_Width_g - 1 downto 0);
@@ -47,25 +48,30 @@ begin
     variable rd_idx_axi   : integer;
   begin
     if rising_edge(Clk) then
-      wr_idx      := to_integer(unsigned(Wr_Addr(Wr_Addr'high downto 2)));
-      rd_idx_axi  := to_integer(unsigned(Rd_Addr(Rd_Addr'high downto 2)));
-
-      -- Write logic
-      if Wr_Ena = '1' and wr_idx < Depth_g then
-        for i in 0 to ByteCount_c - 1 loop
-          if Byte_En(i) = '1' then
-            ram(wr_idx)((i + 1) * 8 - 1 downto i * 8) <= Wr_Data((i + 1) * 8 - 1 downto i * 8);
-          end if;
-        end loop;
-      end if;
-
-      -- Read logic
-      if Rd_Ena = '1' and rd_idx_axi < Depth_g then
-        Rd_Data  <= ram(rd_idx_axi);
-        Rb_valid <= '1';
-      else
+      if Rst = '1' then
         Rd_Data  <= (others => '0');
         Rb_valid <= '0';
+      else
+        wr_idx      := to_integer(unsigned(Wr_Addr(Wr_Addr'high downto 2)));
+        rd_idx_axi  := to_integer(unsigned(Rd_Addr(Rd_Addr'high downto 2)));
+
+        -- Write logic
+        if Wr_Ena = '1' and wr_idx < Depth_g then
+          for i in 0 to ByteCount_c - 1 loop
+            if Byte_En(i) = '1' then
+              ram(wr_idx)((i + 1) * 8 - 1 downto i * 8) <= Wr_Data((i + 1) * 8 - 1 downto i * 8);
+            end if;
+          end loop;
+        end if;
+
+        -- Read logic
+        if Rd_Ena = '1' and rd_idx_axi < Depth_g then
+          Rd_Data  <= ram(rd_idx_axi);
+          Rb_valid <= '1';
+        else
+          Rd_Data  <= (others => '0');
+          Rb_valid <= '0';
+        end if;
       end if;
     end if;
   end process;
@@ -77,13 +83,18 @@ begin
     variable rd_idx_pl : integer;
   begin
     if rising_edge(Clk) then
-      rd_idx_pl := to_integer(unsigned(Rd_Addr_PL));
-      if Rd_En_PL = '1' and rd_idx_pl < Depth_g then
-        Rd_Data_PL  <= ram(rd_idx_pl);
-        Rd_Valid_PL <= '1';
-      else
+      if Rst = '1' then
         Rd_Data_PL  <= (others => '0');
         Rd_Valid_PL <= '0';
+      else
+        rd_idx_pl := to_integer(unsigned(Rd_Addr_PL));
+        if Rd_En_PL = '1' and rd_idx_pl < Depth_g then
+          Rd_Data_PL  <= ram(rd_idx_pl);
+          Rd_Valid_PL <= '1';
+        else
+          Rd_Data_PL  <= (others => '0');
+          Rd_Valid_PL <= '0';
+        end if;
       end if;
     end if;
   end process;
