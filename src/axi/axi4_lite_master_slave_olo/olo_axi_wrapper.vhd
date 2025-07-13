7bb0a4417da6e91e7ba2063ad069d1a9328d060b
Nambinina Rakotojaona <53557693+FPGA-Mada@users.noreply.github.com>
2025-07-13 19:02:34 +0200

    Update olo_axi_wrapper.vhd

--- src/axi/axi4_lite_master_slave_olo/olo_axi_wrapper.vhd
+++ src/axi/axi4_lite_master_slave_olo/olo_axi_wrapper.vhd
@@ -79,7 +79,7 @@ entity olo_axi_wrapper is
 end entity;
 
 architecture rtl of olo_axi_wrapper is
-    constant width_mem : natural := 2**AxiAddrWidth_g;
+    constant width_mem : natural := 200;
     -- Register bus between slave and master
     signal Rb_Addr     : std_logic_vector(AxiAddrWidth_g-1 downto 0);
     signal Rb_Wr       : std_logic;
