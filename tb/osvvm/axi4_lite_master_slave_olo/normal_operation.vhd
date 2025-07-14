3fd49f63fa58c429cdd221a178e6b9662d040a93
Nambinina Rakotojaona <53557693+FPGA-Mada@users.noreply.github.com>
2025-07-14 17:44:33 +0200

    Update normal_operation.vhd

--- tb/osvvm/axi4_lite_master_slave_olo/normal_operation.vhd
+++ tb/osvvm/axi4_lite_master_slave_olo/normal_operation.vhd
@@ -91,7 +91,7 @@ begin
   	 SendRead(SubordinateRec, Addr, RData);
 	 GetWrite(SubordinateRec, Addr_w, RData_w) ;
          AffirmIfEqual(Addr_w,  std_logic_vector(to_unsigned(i * 4, AXI_ADDR_WIDTH)), "Subordinate Write Addr: ") ;
-         AffirmIfEqual(RData_w,  std_logic_vector(to_unsigned(i * 4, AXI_ADDR_WIDTH)), "Subordinate Write Data: ") ;
+         AffirmIfEqual(RData_w,  std_logic_vector(to_unsigned(i * 4, AXI_DATA_WIDTH)), "Subordinate Write Data: ") ;
   	 -- Optional: delay between responses
   	 wait for 10 ns;
    end loop;
